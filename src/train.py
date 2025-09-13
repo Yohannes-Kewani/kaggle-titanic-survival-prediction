aea27f9e32ccafdb63c2903350dbc8f2cf3fc540
Yohannes Kewani Gebrewahid <yohannes.kewani@gmail.com>
2025-09-13 03:44:28 -0700

    Model is modified with voting ensembler with random forest classifier and SVC

--- src/train.py
+++ src/train.py
@@ -1,14 +1,23 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.pipeline import make_pipeline
+
 
 def train_model(df, target='Survived'):
     # Extracting features from train_data
     X = df.drop(columns =[target])
     y = df[target]
     # Splitting the train_data to train set and test set
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
-    # create a linear Regresion 
-    model= LinearRegression()
-    # Train the model
-    model.fit(X_train,y_train)
-    return model, X_test, y_test
+    svc = make_pipeline(StandardScaler(), SVC(gamma='auto', kernel='rbf', C=1, probability=True))
+    score1=cross_val_score(svc,X,y).mean()
+    rfc=RandomForestClassifier(n_estimators = 70)
+    score2=cross_val_score(rfc,X,y).mean()
+    # Create an esemble for improved accuracy
+    vc=VotingClassifier([('clf1',svc),('clf2',rfc)],voting='soft')
+    score3=cross_val_score(vc,X,y).mean()
+    vc.fit(X,y)
+    return vc
